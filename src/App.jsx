4daa87e086334747276962bbc5dfe4049fe5962b
Adam Wathan <4323180+adamwathan@users.noreply.github.com>
2023-08-24 23:04:28 -0400

    Init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+function App() {
+  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
